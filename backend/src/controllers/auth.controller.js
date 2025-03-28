02f72fe40b74d3077ad2868d56971eeda3aa5a1d
TaDat10062002 <talinhdat03@gmail.com>
2025-03-28 10:00:31 +0700

    Add check function and cloundinary library

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { validationResult } from "express-validator";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs";
 import { destroyToken, generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -124,8 +125,9 @@ export const logout = (req, res) => {
 }
 
 export const updateProfile = async (req, res) => {
-    const { profilePic } = req.body;
     try {
+        const { profilePic } = req.body;
+        const userId = req.user._id
         // validate data
         if (!profilePic) {
             return res.status(400).json({
@@ -134,14 +136,23 @@ export const updateProfile = async (req, res) => {
         }
 
         // authorized and update
-        const updatedProfile = await User.findByIdAndUpdate(decoded.userId, { profilePic }, { new: true }).select("-password",)
-        return res.status(200).json({
-            message: "Updated profile successfully",
-            updatedProfile: updatedProfile
-        })
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true })
+        res.status(200).json(updatedUser)
     } catch (error) {
         console.log(`Error updateProfile in controller ${error.message}`);
-        res.status(500).jso({
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+    }
+}
+
+export const checkAuth = async (req, res) => {
+    try {
+        return res.status(200).json(req.user);
+    } catch (error) {
+        console.log(`Error checkAuths in controller ${error.message}`);
+        res.status(500).json({
             message: "Internal Server Error"
         })
     }
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,4 +1,6 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user.model.js"
+
 export const protectedRoute = async (req, res, next) => {
     const token = req.cookies.jwt;
     try {
@@ -14,13 +16,13 @@ export const protectedRoute = async (req, res, next) => {
         const decoded = jwt.decode(token);
         if (!decoded) {
             return res.status(400).json({
-                message: "Invalid token!!!"
+                message: "Unauthorized - Invalid token!!!"
             })
         }
 
         // find user 
-        const _id = decoded._id;
-        const user = await User.findOne(_id);
+        const _id = decoded.userId;
+        const user = await User.findById(_id).select("-password");
 
         // check user exist?
         if (!user) {
@@ -29,7 +31,9 @@ export const protectedRoute = async (req, res, next) => {
             })
         }
         req.user = user;
+        // call the next function
         next();
+
     } catch (error) {
         console.log(`Error protectedRoute in middleware ${error.message}`);
         req.status(500).json({
