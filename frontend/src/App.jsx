77521e95f1f9598aae62ba4403c78f0de7be8129
TaDat10062002 <talinhdat03@gmail.com>
2025-03-31 20:57:04 +0700

    add sign up function for FE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import SettingPage from './pages/SettingPage';
 import ProfilePage from './pages/ProfilePage';
+import { useAuthStore } from './store/useAuthStore';
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth])
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader className='size-10 animate-spin' />
+      </div>
+    )
+  }
+
   return (
-    <>
+    <div>
       <Navbar />
 
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/setting' element={<SettingPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/setting' element={authUser ? <SettingPage /> : <Navigate to="/login" />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
-    </>
+
+      <Toaster />
+    </div>
 
   )
 }
