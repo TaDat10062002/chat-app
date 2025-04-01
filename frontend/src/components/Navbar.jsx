eaa84556c05bf4dd5c6109f324d2f05f4385acc3
TaDat10062002 <talinhdat03@gmail.com>
2025-04-01 23:52:52 +0700

    Add logout function FE and update Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,7 +26,21 @@ const Navbar = () => {
                             <span className='hidden sm:inline'>Settings</span>
                         </Link>
 
+                        {
+                            hehe && (
+                                <>
+                                    <Link to="/profile" className='btn btn-sm gap-2'>
+                                        <User className='size-5' />
+                                        <span className='hidden sm:inline-block'>Profile</span>
+                                    </Link>
 
+                                    <button className='flex gap-2 items-center' onClick={logout}>
+                                        <LogOut className='size-5' />
+                                        <span className='hidden sm:inline-block'>Logout</span>
+                                    </button>
+                                </>
+                            )
+                        }
                     </div>
                 </div>
             </div>
