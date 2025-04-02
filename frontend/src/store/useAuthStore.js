b8971d4d66bd5d34630b379da87eac8f5a152dc0
TaDat10062002 <talinhdat03@gmail.com>
2025-04-02 18:33:54 +0700

    update chat app chat container and relative components

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -8,6 +8,7 @@ export const useAuthStore = create((set) => ({
     isLoggingIn: false,
     isUpdatingProfile: false,
     isCheckingAuth: true,
+    onlineUsers: [],
 
     // set up state
     setAuthUser: (authUser) => set({ authUser }),
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,56 @@
+import { create } from "zustand";
+import { toast } from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+
+export const useChatStore = create((set, get) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUsersLoading: false,
+    isMessagesLoading: false,
+    // todo optimise this one later
+    setSelectedUser: (selectedUser) => set({ selectedUser }),
+
+    getUsers: async () => {
+        set({ isUsersLoading: true })
+        try {
+            const res = await axiosInstance.get("/message/user");
+            set({ users: res.data })
+            toast.success("You can connect with your friends now")
+        } catch (error) {
+            console.log(`Error in getUsers ${error}`);
+            toast.error(error.response.data.messages);
+        }
+        finally {
+            set({ isUsersLoading: false })
+        }
+    },
+
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true })
+        try {
+            const res = await axiosInstance.get(`/message/${userId}`);
+            set({ messages: res.data })
+            toast.success("Message loaded successfully")
+        } catch (error) {
+            console.log(`Error in getMessages ${error}`);
+            toast.error(error.response.data.messages);
+        }
+        finally {
+            set({ isMessagesLoading: false })
+        }
+    },
+
+    sendMessage: async (messageData) => {
+        const { selectedUser, messages } = get();
+        try {
+            const res = await axiosInstance.post(`/message/${selectedUser._id}`, messageData);
+            //update new message and keep all old messages
+            set({ messages: [...messages, res.data] })
+        } catch (error) {
+            console.log(`Error in sendMessage ${error}`);
+            toast.error(error.response.data.messages);
+        }
+    }
+}))
